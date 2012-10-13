71b202a02802f50b5acea51db4e4941b4416f324
Larry Marburger <larry@marburger.cc>
2012-10-13 05:10:36 -0400

    Add config

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source :rubygems
 
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'activesupport'
 gem 'i18n' # stupid activesupport dependency
 
--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 # encoding: utf-8
 require 'sinatra'
+require 'sinatra/config_file'
 require 'sinatra_boilerplate'
+
+config_file 'config.yml'
 
 set :js_assets, %w[zepto.js underscore.js app.coffee]
 
